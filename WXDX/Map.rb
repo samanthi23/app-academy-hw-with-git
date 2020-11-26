0f02ff7316613d3bb9b6c32e8a57f43a6a824d75
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2020-11-26 17:50:33 +0000

    get method returns value if key == key passed in

--- WXDX/Map.rb
+++ WXDX/Map.rb
@@ -9,14 +9,17 @@ class Map
     end
     
     def set(key, value)
-        # if the first element, then element[0] equals key passed in
+        # if the first element, then element[0] equals key 
+        #passed in
         first = @underlying_array.first { | ele | ele[0] == key }
         if first
-            #if first element then array[0][1], values equal value passed in
+            #if first element then array[0][1], values 
+            #equal value passed in
             underlying_array[first][1] = value
         else
             # not first element middle elements
             underlying_array.push([key, value])
+            # [] pushing a array into an array makes it a 2d array
         end 
         # return value
         value
@@ -24,7 +27,12 @@ class Map
     end
 
     def get(key)
-        
+        # review Data Structures lectures @ Harvard. 
+        # returns value array[0][1] if array[0] == key, 
+        #array[0][1] == value else returns 
+        #nil not found
+        underlying_array.each { | ele | returns ele[1] if ele[0] == key }
+        nil
     end
     
     def delete(key)
