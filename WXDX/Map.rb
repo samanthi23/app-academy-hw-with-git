7121ca86cb4c718a35df0d4f57f5a67c339cc345
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2020-11-26 16:47:38 +0000

    created instance methods for map.rb

--- WXDX/Map.rb
+++ WXDX/Map.rb
@@ -1,5 +1,25 @@
 class Map
     #2d array no idea 
     # implements a map using only arrays
+    def initialize(array1, array2)
+     # [['a', 'b']]
+     @ivar = Array.new(array1) { Array.new(array2) }
+    
+    end
+    
+    def set(key, value)
+        
+    end
+
+    def get(key)
+        
+    end
+    
+    def delete(key)
+        
+    end
+    def show 
+        
+    end 
 
 end 
