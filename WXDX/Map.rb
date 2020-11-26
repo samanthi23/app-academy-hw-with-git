b1f82520cb7dfc8152ac7cadd42f77ae7c6d8e08
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2020-11-26 17:31:54 +0000

    added set method to Map.rb create a new key value pair or update

--- WXDX/Map.rb
+++ WXDX/Map.rb
@@ -1,13 +1,25 @@
 class Map
     #2d array no idea 
     # implements a map using only arrays
-    def initialize(array1, array2)
+    def initialize
      # [['a', 'b']]
-     @ivar = Array.new(array1) { Array.new(array2) }
+     #@ivar = Array.new(array1) { Array.new(array2) }
+     @underlying_array = []
     
     end
     
     def set(key, value)
+        # if the first element, then element[0] equals key passed in
+        first = @underlying_array.first { | ele | ele[0] == key }
+        if first
+            #if first element then array[0][1], values equal value passed in
+            underlying_array[first][1] = value
+        else
+            # not first element middle elements
+            underlying_array.push([key, value])
+        end 
+        # return value
+        value
         
     end
 
