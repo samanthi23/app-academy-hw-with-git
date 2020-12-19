fb0930fcdd8ecb6b74c6bfbd01e77a0a91c06e71
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2020-12-19 17:18:34 +0000

    Fixed Queue.rb for enqueue(el)

--- WXDX/Map.rb
+++ WXDX/Map.rb
@@ -1,3 +1,23 @@
 class Map
+    # my_map = [[key1, value1], [key2, value2], [key3, value3]]
     
+    # following instance methods
+    def set(key, value)
+        # if boolean statement here true
+        # return value
+        # key => value
+        
+    end 
+    
+    def get(key)
+        
+    end
+    
+    def delete(key)
+        
+    end 
+    
+    def show
+        
+    end 
 end 
--- WXDX/Queue.rb
+++ WXDX/Queue.rb
@@ -1,12 +1,21 @@
 class Queue
    
+   def initialize
+       @ivar = Array.new
+   end
+   
+   #push for enqueue and pop for dequeue
+   # queue goes right to left 
+   # [[ 3 <= 6 <= 5 ]]
+   # 5 is first
+   
    def enqueue(el)
        # add an item
-       @ivar = Array.new
+       @ivar.push(el)
    end 
    
    def dequeue
-      # remove an item 
+      # remove an item that was here first FIFO
       @ivar.pop
    end
    
