676754415f5a8ebf06f8874ec2d266ed6b2517ad
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2021-03-28 16:37:01 +0000

    added child using child.parent = self

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -4,40 +4,44 @@ class PolyTreeNode
         @parent = nil
         @children = []
     end
-    
+
     def parent
         @parent
     end
-    
+
     def children
         @children.dup
-    end 
-    
+    end
+
     def value
         @value
-    end 
-    
+    end
+
     def parent=(parent)
         return if self.parent == parent # handles nil without issue
-        
+
         # detach
         if self.parent
-           self.parent._children.delete(self) 
+           self.parent._children.delete(self)
         end
-        
+
         @parent = parent
         self.parent._children << self unless self.parent.nil?
-        
+
         self
-        
+
     end
     
+    def add_child(child_node)
+       child_node.parent = self
+    end
+
     def inspect
         @value.inspect
     end
-    
+
     protected # to give node direct access to another node's children
-    
+
     def _children
         @children
     end
