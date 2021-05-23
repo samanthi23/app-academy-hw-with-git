0142d31e41bdf80c16033917c60c57ee8c44db4d
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2021-05-23 19:25:40 +0000

    TicTacToeNode.new

--- skeleton/TicTacToe.md
+++ skeleton/TicTacToe.md
@@ -4,3 +4,29 @@
 
 @next_mover_mark
 
+# board object 
+
+class Board
+
+iterate through all positions that are empty? on board object
+
+create a node by duping board and put next_mover_mark in that positions
+
+prev_move_pos to position you just marked
+
+# DFS
+
+is big-O(b*m) not big-O(b^m) in BFS
+
+# TicTacToeNode
+
+new_board, next_mover_mark :x :o, prev_move_pos (0..2) x, (0..2) y.
+
+new board is board.dup duplicated
+
+# next 
+
+can't move here if it's not free
+
+go back to top of loop
+
--- skeleton/lib/tic_tac_toe_node.rb
+++ skeleton/lib/tic_tac_toe_node.rb
@@ -30,5 +30,25 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    children = []
+    
+    (0..2).each do | row_index|
+      (0..2).each do |column_index|
+        pos = [row_index, column_index]
+    
+    next unless board.empty?(pos)
+    new_board = board.dup
+    new_board[pos] = self.next_mover_mark
+    next_mover_mark = (self.next_mover_mark == :x ? :o : :x)
+    
+    
+    # next mover mark either :x or :o, if :x then :o else :x
+    #new_board[pos]
+    # pos is row index and column index self.next_mover_mark
+    children << TicTacToeNode.new(new_board, next_mover_mark, pos)
   end
+end
+    return children
+  end
+  
 end
